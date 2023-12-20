7c5b7466bb1f889571576895bc7f48a855cceeac
Nidhi Roy <nroy@ex2india.com>
2023-12-20 14:29:49 +0530

    basics variables

---
+++ basics/variables.js
@@ -0,0 +1,10 @@
+const accountId = 12345;
+let accountEmail = "nidhiRoy@google.com";
+const accountState = "jaipur";
+let accountPassword = "0075";
+
+console.table([accountId, accountEmail, accountState, accountPassword]);
+
+accountEmail = "neharoy@gmail.com";
+
+console.log(accountEmail);
